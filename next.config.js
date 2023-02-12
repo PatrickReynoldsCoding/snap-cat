127abf65d14486d494e42d08fb54fd8bf49b594c
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-12 20:53:41 +0000

    trigger deployment

--- next.config.js
+++ next.config.js
@@ -6,6 +6,16 @@ const nextConfig = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
   },
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "cdn2.thecatapi.com",
+        port: "",
+        pathname: "/images/**",
+      },
+    ],
+  },
 };
 
 module.exports = nextConfig;
---
+++ src/pages/api/getCatFact.js
@@ -0,0 +1,13 @@
+export const getCatFact = async () => {
+  try {
+    const res = await fetch(`http://localhost:8080/profile/addfriend/${myId}`, {
+      method: "PATCH",
+      body: JSON.stringify({
+        _id: friend,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+  } catch (err) {}
+};
