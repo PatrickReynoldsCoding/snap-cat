a29980758e1d0abe5750675aa557c32c1304dfea
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-12 19:39:18 +0000

    Styling:cat button

--- src/components/CatCardFact.js
+++ src/components/CatCardFact.js
@@ -1,5 +1,16 @@
 import React from "react";
 import styles from "@/styles/Home.module.css";
 export default function CatCardFact() {
-  return <div className={styles["cat-card-fact"]}></div>;
+  return (
+    <div className={styles["cat-card-fact"]}>
+      <div className={styles["cat-card-fact-text-container"]}>
+        <h1>Cat fact</h1>
+        <h2>Hope you find it furscinating!</h2>
+        <p>
+          "If your cat snores, or rolls over on his back to expose his belly, it
+          means he trusts you."
+        </p>
+      </div>
+    </div>
+  );
 }
--- src/components/NewCatButton.js
+++ src/components/NewCatButton.js
@@ -3,9 +3,11 @@ import styles from "@/styles/Home.module.css";
 
 export default function NewCatButton() {
   return (
-    <div className={styles["new-cat-button"]}>
-      <div className={styles["new-cat-button-icon"]}></div>;
-      <div className={styles["new-cat-button-text"]}></div>;
-    </div>
+    <button className={styles["new-cat-button"]}>
+      <div className={styles["new-cat-button-cross-symbol"]}></div>
+      <div className={styles["new-cat-button-text"]}>New Cat</div>
+      {/* <div className={styles["new-cat-button-icon"]}></div>
+      <div className={styles["new-cat-button-text"]}></div> */}
+    </button>
   );
 }
