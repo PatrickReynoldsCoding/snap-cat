266ca209d3b0687059359d28ed9fca500e8f026a
Patrick Reynolds <patrickreynoldscoding@gmail.com>
2023-02-15 13:28:33 +0000

    trigger-ci

--- src/components/CatCardInterface/Cards/CatCard.js
+++ src/components/CatCardInterface/Cards/CatCard.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "@/styles/Home.module.css";
 
 // Components
-import CatCardImage from "./CatCardImage";
+  import CatCardImage from "./CatCardImage";
 import CatCardFact from "./CatCardFact";
 
 export default function CatCard(props) {
